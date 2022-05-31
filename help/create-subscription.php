7813a63d1fc845b532a1bc620fb839f005ce55d6
Michael Jones <michael@pisignage.co.uk>
2022-05-31 14:28:12 +0100

    Add Help for F1 menu for each page

---
+++ help/create-subscription.php
@@ -0,0 +1,14 @@
+<h2 id="getting-started">Create Subscription</h2>
+<p>A payement subscription is need to for zettle to talk to FPP. For this to work your FPP need to be accsable to the internet.</p>
+<p>This can be done with a service called Dataplicity. Free to use for one device.</p>
+<p>Either <a href="https://www.dataplicity.com/">register</a> or <a href="https://www.dataplicity.com/app">login</a> with Dataplicity.</p>
+<ul>
+    <li>Add Device</li>
+    <li>In FPP to Help > SSL Sell and login with <b>Username</b>: fpp <b>Password</b>: falcon</li>
+    <li>In the sell copy and paste the command and press enter</li>
+    <li>Once the script has been installed go back to Dataplicity Click on 'Your devices' you will see FPP listed</li>
+    <li>Click on the device in the right menu click the secord option and enable 'Wormhole'</li>
+    <li>Once this is done you will see a url. This is what zettle will use to talk to your FPP</li>
+</ul>
+
+NEED TO COMPLETE/EDIT
---
+++ help/setup.php
@@ -0,0 +1,19 @@
+<h2 id="getting-started">Getting Started</h2>
+<p>You'll need an iZettle device to use this plugin.</p>
+<p>Either <a href="https://register.zettle.com/gb">register</a> or <a href="https://login.zettle.com/">login</a> with Zettle.</p>
+<ul>
+    <li>Click 'Integrations' (Bottom Left)</li>
+    <li>Click 'API Keys'</li>
+    <li>Click 'Create API Key'</li>
+    <li>Type a name for your API Key.</li>
+    <li>Select 'READ:USERINFO and READ:PURCHASE'</li>
+    <li>Click 'Create Key'</li>
+</ul>
+<p>You will now be presented with 2 attributes you need to copy &amp; keep safe. They won't be retrievable again so make sure you capture them:</p>
+<ul>
+    <li>client_id</li>
+    <li>API Key</li>
+</ul>
+<h2 id="fpp">FPP</h2>
+<p>Once Announce Zettle is installed, navigate to 'Content Setup' > 'Zettle - Setup'.</p>
+<p>Add your Client ID and Secret to the page and click 'Save'</p>
