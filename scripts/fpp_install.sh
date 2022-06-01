1b7458177298218a1a23f6cd62e64cc0099fb154
Michael Jones <michael@pisignage.co.uk>
2022-06-02 00:30:30 +0100

    Change file owner to fpp so it can be written to

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -19,4 +19,6 @@ EOF
 
 echo "$defalt_json" > /home/fpp/media/config/plugin.fpp-zettle.json
 
+sudo chown fpp /home/fpp/media/config/plugin.fpp-zettle.json
+
 echo "You need a secure https endpoint on your pi to use this plugin. Dataplicity is the easiest way to achieve that. Check out the readme or the plugin help text for more information."
