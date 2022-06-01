953353426e19b6ba7bfa57f924d68893a45af0cd
Michael Jones <michael@pisignage.co.uk>
2022-06-01 23:59:07 +0100

    Write defalt_json to fpp-zettle.json on install

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -7,5 +7,16 @@ echo "Installing Announce Zettle Plugin for FPP...."
 echo "Writing config file...."
 
 touch /home/fpp/media/config/plugin.fpp-zettle.json
+defalt_json=$(cat <<EOF
+{
+  "client_id": "",
+  "client_secret: "",
+  "organizationUuid": "",
+  "subscriptions": ""
+}
+EOF
+)
+
+echo "defalt_json" > /home/fpp/media/config/plugin.fpp-zettle.json
 
 echo "You need a secure https endpoint on your pi to use this plugin. Dataplicity is the easiest way to achieve that. Check out the readme or the plugin help text for more information."
