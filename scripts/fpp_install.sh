06ea27aa5f0f3272b35746e04e2f94d3101e7fbd
Michael Jones <michael@pisignage.co.uk>
2022-06-07 13:22:58 +0100

    Revert ffp_install back to old

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 # fpp-zettle install script
+echo "Installing Announce Zettle Plugin for FPP...."
 
-function writeDefaultJsonToConfig()
-{
+echo "Writing config file...."
+
+file=/home/fpp/media/config/plugin.fpp-zettle.json
+
+touch $file
+
 defalt_json=$(cat <<EOF
 {
    "client_id": "",
@@ -15,23 +20,9 @@ defalt_json=$(cat <<EOF
 }
 EOF
 )
-echo "$defalt_json" > /home/fpp/media/config/plugin.fpp-zettle.json
-}
 
-echo "Installing Announce Zettle Plugin for FPP...."
+echo "$defalt_json" > $file
 
-echo "Writing config file...."
-
-file=/home/fpp/media/config/plugin.fpp-zettle.json
-
-if [ -s "$file" ]
-then
-   echo " Config file exists and is not empty... continuing "
-else
-   echo " Config file does not exist, or is empty "
-   touch $file
-   writeDefaultJsonToConfig()
-   chown fpp /home/fpp/media/config/plugin.fpp-zettle.json
-fi
+sudo chown fpp $file
 
 echo "You need a secure https endpoint on your pi to use this plugin. Dataplicity is the easiest way to achieve that. Check out the readme or the plugin help text for more information."
