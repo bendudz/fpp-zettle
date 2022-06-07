3d26626720a948254f27e59155e3d3b163e8cee5
Ben Dudley <ben@dudleyfamily.org>
2022-06-08 00:55:02 +0100

    Updated status to sort transactions

--- status.php
+++ status.php
@@ -11,114 +11,134 @@ $setupUrl = 'plugin.php?' . http_build_query([
         'plugin' => 'fpp-' . $pluginName,
         'page' => 'setup.php'
     ]);
+
+function sortByTimestampDesc($a, $b)
+{
+    return $b['timestamp'] > $a['timestamp'];
+}
+
+function getTransactions()
+{
+    $data = convertAndGetSettings('zettle-transactions');
+    usort($data, 'sortByTimestampDesc');
+    return $data;
+}
+
+function getStatusData($pj)
+{
+    return [[
+        "client_id" => $pj['client_id'],
+        "client_secret" => substr($pj['client_secret'], 0, 8),
+        "email" => $pj['subscriptions']['contactEmail'],
+        "destination" => $pj['subscriptions']['destination'],
+        "status" => $pj['subscriptions']['status']
+    ]];
+}
+
 ?>
-<style type="text/css">
-    .tg {
-        border-collapse: collapse;
-        border-spacing: 0;
-    }
-
-    .tg td {
-        border-color: black;
-        border-style: solid;
-        border-width: 1px;
-        font-family: Arial, sans-serif;
-        font-size: 14px;
-        overflow: hidden;
-        padding: 10px 5px;
-        word-break: normal;
-    }
-
-    .tg th {
-        border-color: black;
-        border-style: solid;
-        border-width: 1px;
-        font-family: Arial, sans-serif;
-        font-size: 14px;
-        font-weight: normal;
-        overflow: hidden;
-        padding: 10px 5px;
-        word-break: normal;
-    }
-
-    .tg .tg-3xvn {
-        font-family: inherit;
-        font-weight: bold;
-        text-align: center;
-        vertical-align: top
-    }
-
-    .tg .tg-0lax {
-        text-align: left;
-        vertical-align: top
-    }
-
-    .hidetext {
-        -webkit-text-security: disc; /* Default */
-    }
-</style>
-<script type="text/javascript" src="/plugin.php?plugin=fpp-zettle&file=zettle.js&nopage=1"></script>
-<?php if ($pluginJson['client_id'] != '') { ?>
-    <table class="tg">
-        <thead>
-        <tr>
-            <th class="tg-3xvn" colspan="2"></th>
-            <th class="tg-3xvn" colspan="3">Subscriptions</th>
-            <th class="tg-3xvn" colspan="1"></th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <td class="tg-3xvn">Client ID</td>
-            <td class="tg-3xvn">Client Secret</td>
-            <td class="tg-3xvn">Email</td>
-            <td class="tg-3xvn">Destination</td>
-            <td class="tg-3xvn">Status</td>
-            <td class="tg-3xvn">Trigger Effect</td>
-        <tr>
-
-        <tr>
-            <td class="tg-0lax"><?php echo $pluginJson['client_id']; ?></td>
-            <td class="hidetext"><?php echo substr($pluginJson['client_secret'], 0, 8); ?></td>
-            <td class="tg-0lax"><?php echo $pluginJson['subscriptions']['contactEmail']; ?></td>
-            <td class="tg-0lax"><?php echo $pluginJson['subscriptions']['destination']; ?></td>
-            <td class="tg-0lax"><?php echo $pluginJson['subscriptions']['status']; ?></td>
-            <td class="tg-0lax"><?php echo $pluginJson['effect']; ?></td>
-        </tr>
-        </tbody>
-    </table>
-
-    <br><h3>Transactions</h3>
-    <table class="tg">
-        <thead>
-        <tr>
-            <th class="tg-3xvn">Timestamp</th>
-            <th class="tg-3xvn">Amount</th>
-            <th class="tg-3xvn">User Display Name</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?
-        if (count($plugindatabase) > 0) {
-            $total = 0;
-            foreach (array_reverse($plugindatabase) as $d) {
-                // $payload = json_decode($d['payload'], true);
-                echo '<tr>';
-                echo '<td class="tg-0lax">' . date('d/m/y H:i', ceil($d['timestamp'] / 1000)) . '</td>';
-                echo '<td class="tg-0lax">' . $d['formatted_amount'] . '</td>';
-                echo '<td class="tg-0lax">' . $d['userDisplayName'] . '</td>';
-                echo '</tr>';
-                $total += $d['amount'];
-            }
-            echo '<tr><td>Total:</td><td colspan="2">' . number_format($total, 2) . '</td></tr>';
-        } else {
-            echo '<tr><td colspan="3">No Transactions Yet</td></tr>';
+<head>
+    <link
+            href="https://unpkg.com/gridjs/dist/theme/mermaid.min.css"
+            rel="stylesheet"
+    />
+    <style type="text/css">
+        .hidetext {
+            -webkit-text-security: disc; /* Default */
         }
+    </style>
+    <script src="https://unpkg.com/gridjs/dist/gridjs.umd.js"></script>
+</head>
+<body>
+<?php if ($pluginJson['client_id'] != '') { ?>
+<div id="status"></div>
+<script>new gridjs.Grid({
+        columns: [{
+            id: 'client_id',
+            name: 'Client ID'
+        }, {
+            id: 'client_secret',
+            name: 'Client Secret',
+            formatter: (cell) => `******`
+        }, {
+            id: 'email',
+            name: 'Email'
+        }, {
+            id: 'destination',
+            name: 'Destination'
+        }, {
+            id: 'status',
+            name: 'Status'
+        }],
+        resizable: true,
+        style: {
+            table: {
+                border: '3px solid #ccc'
+            },
+            th: {
+                'background-color': 'rgba(0, 0, 0, 0.1)',
+                color: '#000',
+                'border-bottom': '3px solid #ccc',
+                'text-align': 'center'
+            },
+            td: {
+                'text-align': 'center'
+            }
+        },
+        data:
+        <?
+        echo json_encode(getStatusData($pluginJson));
         ?>
-        </tbody>
-    </table>
-    <br>
-    <input id="clear_transactions" class="buttons" value="Clear Transactions">
+
+    }).render(document.getElementById("status"));
+</script>
+<br><br>
+<h3>Transactions</h3>
+<div id="transactions"></div>
+<script>
+    const grid = new gridjs.Grid({
+        columns: [{
+            id: 'timestamp',
+            name: 'Time',
+            width: '33%',
+            formatter: (data) => {
+                return new Date(data).toGMTString();
+            }
+        }, {
+            id: 'amount',
+            name: 'Amount £',
+            width: '33%',
+            formatter: (cell) => `£${cell}`
+        }, {
+            id: 'userDisplayName',
+            name: 'User Display Name',
+            width: '33%'
+        }],
+        sort: true,
+        resizable: true,
+        style: {
+            table: {
+                border: '3px solid #ccc'
+            },
+            th: {
+                'background-color': 'rgba(0, 0, 0, 0.1)',
+                color: '#000',
+                'border-bottom': '3px solid #ccc',
+                'text-align': 'center'
+            },
+            td: {
+                'text-align': 'center'
+            }
+        },
+        data:
+        <?
+        echo json_encode(getTransactions());
+        ?>
+
+    });
+    grid.render(document.getElementById("transactions"));
+</script>
+</body>
 <?php } else { ?>
-<p>You need to configure this plugin before you can see the status. Click here to get to <a href="<?php echo $setupUrl; ?>">setup</a></p>
+    <p>You need to configure this plugin before you can see the status. Click here to get to <a
+                href="<?php echo $setupUrl; ?>">setup</a></p>
 <?php } ?>
