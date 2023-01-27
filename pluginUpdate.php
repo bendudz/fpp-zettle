a88e40ee99309a003d478af37ab082ac82bf4ad8
Michael Jones <michael@pisignage.co.uk>
2023-01-27 23:02:32 +0000

    Bring back plugin update notification

--- pluginUpdate.php
+++ pluginUpdate.php
@@ -1,11 +1,6 @@
 <div id="pluginupdate" class="text-center" style="display: none;">
-        <div class="updateTable text-success fppPluginEntryUpdateStatus">
-            <i class="fas fa-exclamation-circle"></i> <b>Plugin Update Available</b>
-        </div>
-        <button class="buttons btn-success" onclick="UpgradePlugin();"><i class="far fa-arrow-alt-circle-down"></i> Update Now</button>
+    <div class="updateTable text-success fppPluginEntryUpdateStatus">
+        <i class="fas fa-exclamation-circle"></i> <b>Plugin Update Available</b>
     </div>
-<div id='pluginsProgressPopup' title='FPP Plugins' style="display: none">
-    <textarea style='width: 100%;' rows="25"  disabled id='pluginsText'>
-    </textarea>
-    <input id='closeDialogButton' type='button' class='buttons' value='Close' onClick='ClosePluginProgressDialog();' style='display: none;'>
+    <a href="plugins.php" class="buttons btn-success"><i class="far fa-arrow-alt-circle-down"></i> Update Now</a>
 </div>
