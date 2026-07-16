8b000fb458f05b2e4900608898fea13e4341d5d9
Michael Jones <michael@pisignage.co.uk>
2026-07-16 13:29:35 +0100

    Fix config json on install

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -43,10 +43,12 @@ log "Writing default config to $CONFIG"
 	"subscriptions": [],
 	"effect_activate": "no",
 	"command": "",
-	"publish": ["activate": "yes"]
+	"publish": {"activate": "yes"}
 }
 JSONEOF
 fi
+
+shell_exec("sudo chown -R fpp:fpp /home/fpp/media/config/plugin.fpp-zettle.json");
 
 echo "You need a secure https endpoint on your pi to use this plugin. Dataplicity is the easiest way to achieve that. Check out the readme or the plugin help text for more information."
 
